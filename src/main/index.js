ed7aea69d111380b2c03084cc940c568fb30a3a2
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2019-07-04 17:42:34 -0300

    some styles with styled components

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import { Title } from './styles';
+import React, { useState } from 'react';
+import { Container, Form, SubmitButton } from './styles';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../src/services/api';
 
 export default function Main() {
+  const [newRepo, setNewRepo] = useState('rocketseat/unform');
+  const [repositories, setRepositories] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setLoading(true);
+    api
+      .get('/repos/' + newRepo)
+      .then(response => {
+        setRepositories({
+          name: response.data.full_name,
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
   return (
-    <Title>
-      Main
-      <small>menor</small>
-    </Title>
+    <Container>
+      <h1>
+        <FaGithubAlt /> Repositórios
+      </h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          value={newRepo}
+          onChange={event => setNewRepo(event.target.value)}
+          type="text"
+          placeholder="Adicionar repositório"
+        />
+
+        <SubmitButton loading={loading}>
+          {loading ? (
+            <FaSpinner color="#fff" size={14} />
+          ) : (
+            <FaPlus color="#fff" size={14} />
+          )}
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
